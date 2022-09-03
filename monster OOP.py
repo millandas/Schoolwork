f02dc6f6083aafd61e015aec87813c972aee09e3
millandas <99851700+millandas@users.noreply.github.com>
2022-09-03 18:00:02 +0800

    Update monster OOP.py

--- monster OOP.py
+++ monster OOP.py
@@ -10,12 +10,18 @@ class Monster:
         self.intelligence = intelligence
         self.health = health
         self.experience = experience
+listofIDS=[]
 
 with open("monstershard.txt",'r') as file:
     for line in file:
 
-       line = line.strip('\n')
-       parts = line.rsplit(",")
-       print(parts[0])
-       parts[0] = Monster(parts[1],parts[2],parts[3],parts[4],parts[5],parts[6],parts[7],parts[8],parts[9],parts[10])
+        line = line.strip('\n')
+        parts = line.rsplit(",")
+        listofIDS.append(Monster(parts[1],parts[2],parts[3],parts[4],parts[5],parts[6],parts[7],parts[8],parts[9],parts[10]))
 
+ID = int(input("What is the ID of the monster you want to find?"))
+Attribute = input("What stat do you want to know about it?")
+
+Stat = getattr(listofIDS[ID-1],Attribute)
+
+print(Stat)
